7341bd60630b65f16440c0861cec1b41cc708dff
Anthony Chávez <131727293+anthonysurfermx@users.noreply.github.com>
2026-08-23 17:29:55 +0100

    feat: make Bobby app the default landing

--- src/App.tsx
+++ src/App.tsx
@@ -284,7 +284,7 @@ const router = createBrowserRouter(
           index: true,
           element: (
             <Suspense fallback={<PageLoader />}>
-              <BobbyProtocolLanding />
+              <BobbyAppLanding />
             </Suspense>
           ),
         },
